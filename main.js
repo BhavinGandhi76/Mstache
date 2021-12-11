85feaaccccbecee971561d0766536947934dc95e
BhavinGandhi76 <86521436+BhavinGandhi76@users.noreply.github.com>
2021-12-11 17:52:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+   Canvas  = createCanvas(300,300);
+   Canvas.center();
+}
+
+function take_snapshot(){
+    save("Seriously.png");
+}
+function draw(){
+
+}
